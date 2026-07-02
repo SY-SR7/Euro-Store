cd18d2500a14a0ff0d852e47494ccb1af44f64f2
Ammar <ammar@eurostore.com>
2026-07-02 14:07:14 +0200

    auto-sync: 2026-07-02 14:07:14

--- apps/helper/src/app/api/orders/[id]/route.ts
+++ apps/helper/src/app/api/orders/[id]/route.ts
@@ -1,6 +1,6 @@
-﻿import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
-import { createServerSupabaseClient } from '../../../../../supabase-server';
+import { createServerSupabaseClient } from '../../../../supabase-server';
 import { z } from 'zod';
 
 const schema = z.object({
